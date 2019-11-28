1a2e9fd0c593df937cc9e6a66fbcd82a128afdd6
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-11-28 08:58:12 +0100

    update app

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,20 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
-  document.body.style.background = "#000"
-  document.body.style.color = "#fff !important"
-  
-  sendResponse({msg:'background changed'})
+  switch (request) {
+    case 'changeBack':
+      document.body.style.background = "#000"
+      document.body.style.color = "#fff !important"
+
+      sendResponse({ msg: 'background changed' })
+      break;
+
+    case 'jsonParse':
+      var getPre = document.querySelector('pre')
+      getPre.textContent = JSON.stringify(JSON.parse(getPre.textContent), null,2)
+      
+      break;
+
+    default:
+      break;
+  }
 })
--- popup.js
+++ popup.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   let btnTopSites = document.getElementById('top-sites')
   let changeColor = document.getElementById('changeColor');
+
+  let btnSaveForLater = document.getElementById('save-for-later');
+  let btnGetStorage = document.getElementById('get-storage')
+
+  let btnJsonParse = document.getElementById('json-parse')
+
   let ulList = document.getElementById('list')
 
 
@@ -22,10 +28,33 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     };
 
-    function onBackChange(resp) {
-      alert(resp.msg);
-      
-    }
+    function onBackChange (resp) { }
+  }
+
+
+  if (btnSaveForLater) {
+    btnSaveForLater.addEventListener('click', () => {
+      alert('ok')
+      chrome.storage.sync.set({ 'key': 'hello storage' }, function () {
+        //alert('Value is set to ');
+      });
+    })
+  }
+
+  if (btnGetStorage) {
+    btnGetStorage.addEventListener('click', () => {
+      chrome.storage.sync.get(['key'], function (result) {
+        alert('Value currently is ' + result.key);
+      });
+    })
+  }
+
+  if (btnJsonParse) {
+    btnJsonParse.addEventListener('click', () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'jsonParse')
+      });
+    })
   }
 
 })
