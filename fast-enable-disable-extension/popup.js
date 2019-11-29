c382bad6dd954c1ae289ba8bcc687ec024950a3f
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-11-29 15:52:33 +0100

    update app

--- fast-enable-disable-extension/popup.js
+++ fast-enable-disable-extension/popup.js
@@ -1,48 +1,47 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  let btnExtensions = document.getElementById('extensions')
-  let ulList = document.getElementById('list')
+  var filterInput = document.getElementById('filter')
+  var ulList = document.getElementById('list')
 
-  if (btnExtensions) {
-    btnExtensions.addEventListener('click', () => {
-      // id, name, icons, enabled, isApp, description
-      let extInfo = {}
+  // id, name, icons, enabled, isApp, description          
+  chrome.management.getAll(function (result) {
 
-      // chrome.management.setEnabled(string id, boolean enabled, function callback)
-
-      chrome.management.getAll(function (result) {
-
-        ulList.innerHTML = ''
-        result.forEach(extension => {
-          ulList.innerHTML += `      
-<div class="list-group-item d-flex justify-content-between align-items-center">
-  <p class="m-0">${extension.name} <span class="badge badge-primary">${extension.isApp?'app':'extension'}</span></p>
-  <small class="text-muted">      
-    ${extension.enabled ?
-          `<input class="form-check-input" type="checkbox" data-id="${extension.id}" checked>`
-          : `<input class="form-check-input" type="checkbox" data-id="${extension.id}">`}    
-  </small>
-</div>`});
-
-        let chkkk = document.querySelectorAll('.form-check-input')
-
-        Object.keys(chkkk).forEach(v => {
-          chkkk[v].addEventListener('change', (e) => {
-
-            const extensionId = e.target.dataset.id;
-            const status = e.target.checked
-            chrome.management.setEnabled(extensionId, status, () => {
-              console.log('disbaled  : ' + status + ' : ' + extensionId);
-            })
-
-          })
-
-        }) // object loop
-
-      }) // end getAll
-
-
+    filterInput.addEventListener('keyup', (e) => {
+      let newResult = result.filter(r => r.name.toLowerCase().includes((e.target.value).toLowerCase()))
+      createList(newResult && newResult.length > 0 ? newResult : result)
+      enableDisableExt(document.querySelectorAll('.form-check-input'))
     })
+
+    createList(result)
+    enableDisableExt(document.querySelectorAll('.form-check-input'))
+  }) // end getAll
+
+
+  function enableDisableExt (chkkk) {
+    Object.keys(chkkk).forEach(v => {
+      chkkk[v].addEventListener('change', (e) => {
+        const extensionId = e.target.dataset.id;
+        const status = e.target.checked
+        // chrome.management.setEnabled(string id, boolean enabled, function callback)
+        chrome.management.setEnabled(extensionId, status)
+      })
+    }) // object loop
+  }
+
+  function createList (extensions) {
+    ulList.innerHTML = ''
+    extensions.forEach(extension => {
+      ulList.innerHTML += `      
+<div class="list-group-item d-flex justify-content-between align-items-center">
+  <p class="m-0">${extension.name} <span class="badge badge-primary">${extension.isApp ? 'app' : 'extension'}</span></p>
+    
+    <div>
+    ${extension.enabled ?
+      `<input class="form-check-input mt-0" type="checkbox" data-id="${extension.id}" checked>`
+      : `<input class="form-check-input mt-0" type="checkbox" data-id="${extension.id}">`}
+    </div>    
+
+</div>`});
   }
 
 })
