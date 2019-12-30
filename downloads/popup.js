c4fcb4c9cfb1de92d48c3ddc18899f33e5363e7f
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-12-30 03:00:33 +0100

    update

--- downloads/popup.js
+++ downloads/popup.js
@@ -1,31 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  let ulList = document.getElementById('list')
-
-  // chrome.topSites.get(function (data) {
-  //   ulList.innerHTML = ''
-  //   data.forEach(element => {
-  //     ulList.innerHTML += `
-  //         <a href="${element.url}" class="list-group-item">🔗 ${element.title}</a>`
-  //   });
-  // })
+  let ulList = document.getElementById('list');
 
   // download : id, filename, state: "complete", exists: true, fileSize: 10219, mime: "application/zip"
   chrome.downloads.search({}, (result) => {
-    ulList.innerHTML = ''
-    result = result.slice(0, 10)
-    result.forEach(element => {
-      ulList.innerHTML += `<li class="list-group-item">🔗 ${getItemName(element.filename)}</li>`
-    });
 
+    createListDownloads(result, ulList);
+
+    // remove item from list of downloads
+    setTimeout(() => {
+      document.querySelectorAll('.btn-remove').forEach(b => {
+        b.addEventListener('click', () => {
+          let itemId = b.dataset.id;
+          chrome.downloads.erase({ id: parseInt(itemId, 10) }, function () {
+            document.getElementById(`${itemId}`).style.display = 'none';
+          });
+        });
+      })
+    }, 500);
+
+    // open folder    
+    setTimeout(() => {
+      document.querySelectorAll('.item').forEach(b => {
+        b.addEventListener('click', () => {
+          let itemId = b.dataset.id;
+          chrome.downloads.show(parseInt(itemId, 10));
+        });
+      })
+    }, 500);
   });
 
-  // filename: "C:\Users\haike\Downloads\tabCapture.zip" => tabCapture
+
+  function createListDownloads (result, ulList) {
+    ulList.innerHTML = '';
+    let slicedRes = result && result.length > 19 ? result.slice(0, 20) : result;
+    slicedRes.forEach(element => {
+      ulList.innerHTML += `<li class="list-group-item" id="${element.id}">
+        <p class="item" data-id="${element.id}">${getItemName(element.filename)}</p>
+        <button class="btn-remove" data-id="${element.id}">REMOVE</button>
+      </li>`;
+    });
+  }
+
+  // /[^\\.]+(?=\.)/gi : "C:\Users\haike\Downloads\tabCapture.zip" => tabCapture.zip
   function getItemName (filename) {
-    if (filename) {
-      filename = String.raw`${filename}`
-      let extFileName = filename.match(/[^\\.]+(?=\.)/gi)[0]
-      if (extFileName !== undefined || extFileName.length > 0) {
+    if (filename && filename.length > 0) {
+      filename = String.raw`${filename}`;
+      let extFileName = filename.split('\\').reverse()[0];
+      if (extFileName && (extFileName !== undefined || extFileName.length > 0)) {
         return extFileName
       }
     }
